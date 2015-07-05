9287316e1f00399bbfb8481a3428a5050782dda9
jtfmumm <jtfmumm@gmail.com>
2015-07-05 14:42:03 -0400

    Add test cases

--- src/main/scala/AS.scala
+++ src/main/scala/AS.scala
@@ -1,6 +1,8 @@
 package main.scala
 import algstructs._
+import algprops._
 import org.scalacheck._
+import org.scalacheck.Arbitrary._
 
 object AS {
   def main(args: Array[String]) = {
@@ -11,6 +13,13 @@ object AS {
       def op(a: Int, b: Int): Int = a + b
     }
 
+    case class IntDivide extends AbelianGroup[Int] {
+      val e = 1
+      def inv(a: Int): Int = a
+
+      def op(a: Int, b: Int): Int = a / b
+    }
+
     case class StringConcat extends Monoid[String] {
       val e = ""
       def op(a: String, b: String): String = a + b
@@ -18,8 +27,24 @@ object AS {
 
     val i = new IntAdd
     println(i.op(1, 2))
-    val intList = Gen.listOf(Gen.choose(0, 100))
-    println(intList)
+    val int2List = Gen.listOfN(2, Gen.choose(0, 100)).suchThat(_.size == 2)
+    val int3List = Gen.listOfN(3, Gen.choose(0, 100)).suchThat(_.size == 3)
+    val string2List = Gen.listOfN(3, arbitrary[String]).suchThat(_.size == 2)
+    val string3List = Gen.listOfN(3, arbitrary[String]).suchThat(_.size == 3)
+    println("IntAdd: defined")
+    println(AlgProperties.definedForAllElementsOn[Int](IntAdd(), int2List).check)
+    println("IntDivide: defined")
+    println(AlgProperties.definedForAllElementsOn[Int](IntDivide(), int2List).check)
+    println("StringConcat: defined")
+    println(AlgProperties.definedForAllElementsOn[String](StringConcat(), string2List).check)
+    println("StringConcat: assoc")
+    println(AlgProperties.associativityOn[String](StringConcat(), string3List).check)
+    println("IntAdd: assoc")
+    println(AlgProperties.associativityOn[Int](IntAdd(), int3List).check)
+    println("StringConcat: comm")
+    println(AlgProperties.commutativityOn[String](StringConcat(), string2List).check)
+    println("IntAdd: comm")
+    println(AlgProperties.commutativityOn[Int](IntAdd(), int3List).check)
   }
 
 }
--- src/main/scala/algprops/AlgProperties.scala
+++ src/main/scala/algprops/AlgProperties.scala
@@ -3,11 +3,11 @@ import org.scalacheck.Prop._
 import org.scalacheck._
 import main.scala.algstructs._
 
-class AlgProperties {
+object AlgProperties {
   // closure and injection are assured by the fact that in this implementation op is always a function of the form (A, A) => A
-  def definedForAllElementsOn[A](as: Magma[A], genFor: Gen[(A, A)]) = forAll(genFor)(t => {
-    val x = t._1
-    val y = t._2
+  def definedForAllElementsOn[A](as: Magma[A], genFor: Gen[List[A]]) = forAll(genFor)(l => {
+    val x = l(0)
+    val y = l(1)
     try {
       as.op(x, y)
       true
@@ -16,15 +16,15 @@ class AlgProperties {
       case _: Throwable => false
     }
   })
-  def associativityOn[A](as: Magma[A], genFor: Gen[(A, A, A)]) = forAll(genFor)(t => {
-    val x = t._1
-    val y = t._2
-    val z = t._3
+  def associativityOn[A](as: Magma[A], genFor: Gen[List[A]]) = forAll(genFor)(l => {
+    val x = l(0)
+    val y = l(1)
+    val z = l(2)
     as.op(as.op(x, y), z) == as.op(x, as.op(y, z))
   })
-  def commutativityOn[A](as: Magma[A], genFor: Gen[(A, A)]) = forAll(genFor)(t => {
-    val x = t._1
-    val y = t._2
+  def commutativityOn[A](as: Magma[A], genFor: Gen[List[A]]) = forAll(genFor)(l => {
+    val x = l(0)
+    val y = l(1)
     as.op(x, y) == as.op(y, x)
   })
   def identityOn[A](as: Monoid[A], genFor: Gen[A]) = forAll(genFor)(x => {
